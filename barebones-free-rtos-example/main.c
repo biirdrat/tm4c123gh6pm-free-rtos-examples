a93b5b41f1b555c7d7280e862b77dc8115639114
biirdrat <steventubusiness@gmail.com>
2025-09-20 02:43:50 -0500

    docs: change barebones example code spacing

--- barebones-free-rtos-example/main.c
+++ barebones-free-rtos-example/main.c
@@ -21,6 +21,7 @@
 // This is part of revision 2.1.4.178 of the EK-TM4C123GXL Firmware Package.
 //
 //*****************************************************************************
+
 #include <stdbool.h>
 #include <stdint.h>
 #include "inc/hw_memmap.h"
@@ -35,13 +36,7 @@
 #include "task.h"
 #include "queue.h"
 #include "semphr.h"
-//*****************************************************************************
-//
-// The mutex that protects concurrent access of UART from multiple tasks.
-//
-//*****************************************************************************
-xSemaphoreHandle UARTSemaphore;
-SemaphoreHandle_t xTask1Semaphore;
+
 
 //*****************************************************************************
 //
@@ -61,22 +56,16 @@ __error__(char *pcFilename, uint32_t ui32Line)
 // This hook is called by FreeRTOS when an stack overflow error is detected.
 //
 //*****************************************************************************
-void vApplicationStackOverflowHook(xTaskHandle *pxTask, char *pcTaskName)
+void
+vApplicationStackOverflowHook(xTaskHandle *pxTask, char *pcTaskName)
 {
+    //
     // This function can not return, so loop forever.  Interrupts are disabled
     // on entry to this function, so no processor interrupts will interrupt
     // this loop.
+    //
     while(1)
     {
-    }
-}
-
-void vTask1(void* pvParameters)
-{
-    while (1)
-    {
-    	GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_1) ^ GPIO_PIN_1);
-    	vTaskDelay(500);
     }
 }
 
@@ -90,18 +79,26 @@ void initializeGPIOFLeds()
 	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
 }
 
+void vTask1(void* pvParameters)
+{
+	while(1)
+	{
+		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_1) ^ GPIO_PIN_1);
+
+		vTaskDelay(500);
+	}
+}
+
 int main(void)
 {
-    // Set the clocking to run at 40 MHz from the PLL.
+	// Set Clock Speed to 40MHz
     SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
 
     initializePeripherals();
 
     initializeGPIOFLeds();
 
-	UARTSemaphore = xSemaphoreCreateMutex();
-
-	xTaskCreate(vTask1, "Task 1", 256, NULL, 10, NULL);
+    xTaskCreate(vTask1, "Task 1", 256, NULL, 10, NULL);
 
     vTaskStartScheduler();
 
